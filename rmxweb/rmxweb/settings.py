e583d12dbecd4e6aaeaf134f0bdab10462027eff
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-05-16 15:01:42 +0100

    -

--- rmxweb/rmxweb/settings.py
+++ rmxweb/rmxweb/settings.py
@@ -12,15 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from . import config
-
-# importing celery configurations to make them available in
-# django.conf.settings
-from . import celery_settings
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
