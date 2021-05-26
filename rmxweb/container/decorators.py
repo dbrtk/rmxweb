0a33df45a0b7dd06cbd4aee792383ae6342ef017
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-05-26 22:47:09 +0100

    -

--- rmxweb/container/decorators.py
+++ rmxweb/container/decorators.py
@@ -32,9 +32,7 @@ def feats_available(func):
                      featsperdoc: int = 3, **kwds):
 
         container = Container.get_object(pk=containerid)
-        print(f'\n\navailable feats; container: {container}')
         availability = container.features_availability(feature_number=features)
-        print(f'availability: {availability}')
         out = {
             'busy': True,
             'retry': True,
@@ -44,9 +42,7 @@ def feats_available(func):
             'containerid': container.pk
         }
         if availability.get('busy'):
-            print(f'return is busy: {out}')
             return out
-        print(f"is not busy; is available: {availability.get('available')}")
         if availability.get('available'):
             out = {
                 'words': words,
@@ -56,10 +52,7 @@ def feats_available(func):
                 'container': container,
             }
             out.update(kwds)
-            print(f'is available, out: {out}')
-            print(f'the function being returned: {func}')
             return func(out)
-        print(f'CALLING RMXWEB_TASKS[\'generate_matrices_remote\']')
         celery.send_task(
             config.RMXWEB_TASKS['generate_matrices_remote'],
             kwargs={
