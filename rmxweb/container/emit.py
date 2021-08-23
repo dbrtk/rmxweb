e085ab917d192960b6b130114488663035f00a48
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-08-24 00:14:50 +0100

    updating crawl_async and removing generate_matrices_remote

--- rmxweb/container/emit.py
+++ rmxweb/container/emit.py
@@ -1,10 +1,6 @@
 """Emitting miscellaneous messages to the world (other services) through
    celery/rabbitmq.
 """
-import os
-
-from prom.config import COMPUTE_MATRIX_CALLBACK_PREFIX, COMPUTE_MATRIX_RUN_PREFIX
-from prom.decorator import trackprogress
 from rmxweb.celery import celery
 from rmxweb.config import (
     CRAWL_START_MONITOR_COUNTDOWN,
@@ -25,24 +21,27 @@ def get_available_features(containerid: int = None, folder_path: str = None):
     ).get()
 
 
-def crawl_async(url_list: list = None, containerid=None, depth=1):
+def crawl_async(url_list: list = None, container=None, depth=1):
     """Starting the crawler in scrasync. Starting the task that will monitor
        the crawler.
     """
+    if container.container_ready:
+        container.toggle_container_ready(crawl_ready=False)
     crawlid = celery.send_task(SCRASYNC_TASKS['launch_crawl'], kwargs={
         'endpoint': url_list,
-        'containerid': containerid,
+        'containerid': container.pk,
         'depth': depth
     }).get()
-
-    # todo(): delete this - no monitoring is needed.
     # the countdown argument is here to make sure that this task does not
     # start immediately as prometheus may be empty.
-    # celery.send_task(
-    #     RMXWEB_TASKS['monitor_crawl'],
-    #     kwargs={'containerid': containerid, 'crawlid': crawlid},
-    #     countdown=CRAWL_START_MONITOR_COUNTDOWN
-    # )
+    celery.send_task(
+        RMXWEB_TASKS['monitor_crawl'],
+        kwargs={
+            'containerid': container.pk,
+            'crawlid': crawlid
+        },
+        countdown=CRAWL_START_MONITOR_COUNTDOWN
+    )
     return crawlid
 
 
@@ -65,36 +64,3 @@ def get_features(feats: int = 10,
             'feats_per_doc': feats_per_doc
         }
     ).get()
-
-
-@trackprogress(dtype=COMPUTE_MATRIX_RUN_PREFIX)
-def generate_matrices_remote(
-        container=None,
-        feats: int = 10,
-        words: int = 6,
-        vectors_path: str = None,
-        docs_per_feat: int = 0,
-        feats_per_doc: int = 3):
-    """
-    Generating matrices on the remote server.
-
-    :param container: instance of Container
-    :param feats:
-    :param words:
-    :param vectors_path:
-    :param docs_per_feat:
-    :param feats_per_doc:
-    """
-    containerid = container.pk
-    kwds = {
-        'containerid': containerid,
-        'feats': int(feats),
-        'words': words,
-        'docs_per_feat': int(docs_per_feat),
-        'feats_per_doc': int(feats_per_doc),
-        'path': container.get_folder_path(),
-    }
-    if os.path.isfile(vectors_path):
-        celery.send_task(NLP_TASKS['factorize_matrices'], kwargs=kwds)
-    else:
-        celery.send_task(NLP_TASKS['compute_matrices'], kwargs=kwds)
