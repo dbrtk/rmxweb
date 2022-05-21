02a8ea50e4f87cc868e73ee0fe9d5ab864adfe96
Dominik Bartkowski <dominikbartkowski@gmail.com>
2022-05-21 19:10:05 +0100

    -

--- rmxweb/rmxweb/celery_settings.py
+++ rmxweb/rmxweb/celery_settings.py
@@ -2,17 +2,6 @@
 import os
 import re
 
-# todo(): delete rabbit related stuff
-# RabbitMQ login credentials
-# RPC_PASS = os.environ.get('RABBITMQ_DEFAULT_PASS')
-# RPC_USER = os.environ.get('RABBITMQ_DEFAULT_USER')
-# RPC_VHOST = os.environ.get('RABBITMQ_DEFAULT_VHOST')
-
-# the host to which the rpc broker (rabbitmq) is deployed
-# RPC_HOST = os.environ.get('RABBITMQ_HOST')
-# RPC_PORT = os.environ.get('RABBITMQ_PORT', 5672)
-# RPC_QUEUE_NAME = os.environ.get('RPC_QUEUE_NAME', 'rmxweb')
-
 
 # REDIS CONFIG
 # celery, redis (auth access) configuration. Reids is being used as a result
@@ -22,13 +11,7 @@ REDIS_PASS = os.environ.get('REDIS_PASS')
 REDIS_DB_NUMBER = os.environ.get('REDIS_DB_NUMBER')
 REDIS_PORT = os.environ.get('REDIS_PORT')
 
-
-# todo(): delete these
-# broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost'
-# _url = f'amqp://{RPC_USER}:{RPC_PASS}@{RPC_HOST}:{RPC_PORT}/{RPC_VHOST}'
-
 _redis_url = f'redis://:{REDIS_PASS}@{BROKER_HOST_NAME}:{REDIS_PORT}/{REDIS_DB_NUMBER}'
-
 
 broker_url = _redis_url
 
