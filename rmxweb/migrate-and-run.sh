97216128e3cb3c2bca1dfc1a7e42afff78f17c49
Dominik Bartkowski <dominikbartkowski@gmail.com>
2022-06-26 13:35:33 +0100

    -

--- rmxweb/migrate-and-run.sh
+++ rmxweb/migrate-and-run.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-until python3 check_db_availability.py; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
+#until python3 check_db_availability.py; do
+#  >&2 echo "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#>&2 echo "Postgres is up - executing command"
 
 >&2 echo "Running manage.py migrate"
 python3 manage.py migrate
--- rmxweb/rmxweb/settings.py
+++ rmxweb/rmxweb/settings.py
@@ -76,15 +76,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'rmxweb.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USERNAME'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.environ.get("DATABASE_NAME"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DATABASE_NAME'),
+#         'USER': os.environ.get('DATABASE_USERNAME'),
+#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+#         'HOST': os.environ.get('DATABASE_HOST'),
+#         'PORT': os.environ.get('DATABASE_PORT', '5432'),
+#     }
+# }
 
 
 # Password validation
